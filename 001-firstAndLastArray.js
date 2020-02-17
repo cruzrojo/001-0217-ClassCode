1f9884e3bd700c9d0c936507fdc3596fdb4f578e
cruzrojo <burkerich@gmail.com>
2020-02-17 13:51:28 -0700

    added pseudocode in a .txt file. Added my code and Jordan's solution to the code as well in a file called .001-firstAndLastArray.js

---
+++ 001-firstAndLastArray.js
@@ -0,0 +1,20 @@
+let myArray = ['bad shit', 'good shit', 'really good shit', 'this is bad shit']
+console.log(myArray)
+
+let theGood = myArray.slice(1, 3)
+console.log(theGood)
+
+// let theBad = myArray.pop() + myArray.pop(0);
+// console.log(theBad)
+
+const removeFirstAndLast = arr => {
+    if (arr.length >= 3) {
+      return arr.slice(1, -1);
+    } else {
+      throw "You need at least three elements in the array";
+    }
+  };
+  
+  removeFirstAndLast([1, 2, 3, 4]); //?
+  removeFirstAndLast(["<h1>", "Some content", "</h1>"]); //?
+  removeFirstAndLast(["Some content", "</h1>"]); //?
